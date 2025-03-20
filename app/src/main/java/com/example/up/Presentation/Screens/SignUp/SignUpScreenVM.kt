0fb5b41af62cacbadca8597b4b4348fdf0d6f69e
Lera <batrakovavv12@gmail.com>
2025-03-20 21:07:11 +0300

    Add sign up fun

--- app/src/main/java/com/example/up/Presentation/Screens/SignUp/SignUpScreenVM.kt
+++ app/src/main/java/com/example/up/Presentation/Screens/SignUp/SignUpScreenVM.kt
@@ -1,31 +1,31 @@
-package com.example.up.Presentation.Screens.SignIn
-
+package com.example.up.Presentation.Screens.SignUp
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import com.example.up.Domain.Constant
 import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.auth.providers.builtin.Email
-import kotlinx.coroutines.launch
 
+class SignUpScreenVM:ViewModel() {
 
-class SignInScreenVM : ViewModel() {
-
-    fun onSignInEmailPassword(emailUser: String, passwordUser: String) {
+    fun onSignUpEmail(emailUser: String, passwordUser: String) {
         viewModelScope.launch {
-            try {
-                val user = Constant.supabase.auth.signInWith(Email){
+            try{
+                var  user =  Constant.supabase.auth.signUpWith(Email) {
+
                     email = emailUser
                     password = passwordUser
                 }
                 println(user.toString())
                 println(Constant.supabase.auth.currentUserOrNull()!!.id)
                 println("Success")
-            } catch (e: Exception) {
+            }
+            catch (e: Exception) {
                 println("Error")
                 println(e.message.toString())
             }
+
         }
     }
-
 }
